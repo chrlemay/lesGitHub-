bdad61b16bbe982bd95d73dc4bc1e613b597b046
chrlemay <chrlemay@gmail.com>
2018-10-17 19:43:04 +0200

    Add project files.

---
+++ Les_console_git/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Les_console_git
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
